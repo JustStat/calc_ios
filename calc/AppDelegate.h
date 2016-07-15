8e682177e660657a225f07d2e95bf66fd7e0500c
Kirill Varlamov <k.varlamov@mayak.red>
2016-07-15 23:34:56 +1000

    Initial Commit

---
+++ calc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calc
+//
+//  Created by Kirill Varlamov on 15.07.16.
+//  Copyright © 2016 Kirill Varlamov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calc
+//
+//  Created by Kirill Varlamov on 15.07.16.
+//  Copyright © 2016 Kirill Varlamov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
